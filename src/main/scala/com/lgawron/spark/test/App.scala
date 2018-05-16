1c1d43af73cd27fe0578be9ab540409e18267415
lukasz.gawron <lukasz.gawron@performgroup.com>
2018-05-16 23:11:01 +0200

    add basic App.scala hello world class, and basic AppTest class to confirm that test framework is configured, and basic app can be started without errors

---
+++ src/main/scala/com/lgawron/spark/test/App.scala
@@ -0,0 +1,10 @@
+package com.lgawron.spark.test
+
+/**
+  * Created by lukasz.gawron on 16/05/2018.
+  */
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
---
+++ src/test/scala/com/lgawron/spark/test/AppTest.scala
@@ -0,0 +1,12 @@
+package com.lgawron.spark.test
+
+import org.scalatest.FunSpec
+
+/**
+  * Created by lukasz.gawron on 16/05/2018.
+  */
+class AppTest extends FunSpec {
+  it("should print hello world") {
+    assert(true)
+  }
+}
