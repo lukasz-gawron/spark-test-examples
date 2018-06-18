39489f65b7fb19971fffd7fb2f1ef51502b29879
lukasz.gawron <lukasz.gawron@performgroup.com>
2018-06-18 20:50:05 +0200

    remove second part of streaming test, only one window will be tested.

--- src/test/scala/com/lgawron/spark/test/S05_StreamingTest.scala
+++ src/test/scala/com/lgawron/spark/test/S05_StreamingTest.scala
@@ -2,6 +2,7 @@ package com.lgawron.spark.test
 
 import com.lgawron.spark.test.WordsCount.WordCount
 import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.{Time, Seconds}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.{Millis, Span}
@@ -27,8 +28,9 @@ class S05_StreamingTest extends SparkStreamingBaseSpec with Eventually {
 
     var results = ListBuffer.empty[Array[WordCount]]
 
+    val linesDStream: InputDStream[String] = ssc.queueStream(lines)
     WordsCountStreaming.count(ssc,
-      ssc.queueStream(lines),
+      linesDStream,
       windowDuration,
       slideDuration) { (wordsCount: RDD[WordCount], time: Time) =>
       results += wordsCount.collect()
@@ -46,40 +48,5 @@ class S05_StreamingTest extends SparkStreamingBaseSpec with Eventually {
         WordCount("a", 1),
         WordCount("b", 1)))
     }
-
-    When("second set of words queued")
-    lines += ss.sparkContext.makeRDD(Seq("b", "c"))
-
-    Then("words counted after second slide")
-    advanceClock(slideDuration)
-    eventually {
-      results.last should equal(Array(
-        WordCount("a", 1),
-        WordCount("b", 2),
-        WordCount("c", 1)))
-    }
-
-    When("nothing more queued")
-
-    Then("word counted after third slide")
-    advanceClock(slideDuration)
-    eventually {
-      results.last should equal(Array(
-        WordCount("a", 0),
-        WordCount("b", 1),
-        WordCount("c", 1)))
-    }
-
-    When("nothing more queued")
-
-    Then("word counted after fourth slide")
-    advanceClock(slideDuration)
-    eventually {
-      results.last should equal(Array(
-        WordCount("a", 0),
-        WordCount("b", 0),
-        WordCount("c", 0)))
-    }
   }
-
 }
