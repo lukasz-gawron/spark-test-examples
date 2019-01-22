ed3796803405d7b6a00abb8c861eb88785d6341f
lukasz.gawron <lukasz.gawron@performgroup.com>
2019-01-22 11:46:39 +0100

    add pending method for KontextFrei usage

--- src/test/scala/com/lgawron/spark/test/S07_KontextFreiTest.scala
+++ src/test/scala/com/lgawron/spark/test/S07_KontextFreiTest.scala
@@ -1,8 +1,15 @@
 package com.lgawron.spark.test
 
+import org.scalatest.{FunSpec, FunSuite}
+
 /**
   * Created by lukasz.gawron on 18/06/2018.
+  * https://github.com/dwestheide/kontextfrei
   */
-class S07_KontextFreiTest {
+class S07_KontextFreiTest extends FunSpec {
 
+  describe("kontexfrei examples") {
+
+    it("should provide example of kontextfrei usage for Spark testing") (pending)
+  }
 }
